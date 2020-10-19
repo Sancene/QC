1b20e327f79a9eda136758f974dcf30f3575ab38
Sancene <minefattoproject@gmail.com>
2020-10-19 14:22:21 +0300

    lab 3 added

---
+++ lab3.1/lab3.1/CodeFile1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LW5.MyString
+{
+    public partial class MyString
+    {
+        private class MyStringEnumerator : IEnumerator<char>
+        {
+            private readonly char[] _data;
+            internal MyStringEnumerator(char[] data)
+            {
+                _data = data;
+                Reset();
+            }
+
+            public bool MoveNext()
+            {
+                if (_currentIndex >= _data.Length)
+                    return false;
+
+                Current = _data[_currentIndex];
+                _currentIndex++;
+                return true;
+            }
+
+            public void Reset()
+            {
+                _currentIndex = 0;
+            }
+
+            private int _currentIndex;
+
+            public char Current { get; private set; }
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
